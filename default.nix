b6af809e8ea6a0227b146e67d8862bb4d5f8179e
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-05-13 15:56:45 +0300

    Fix filter

--- default.nix
+++ default.nix
@@ -1,18 +1,64 @@
-{ stdenv, cacert, lib, nodePackages }:
+{ stdenv, cacert, lib, nodePackages, nodejs }:
+# with import <nixpkgs> {};
 
-stdenv.mkDerivation {
-  name = "playground-deps";
+let
+  version = "0.1.0";
+
+  nix-filter = import ./nix-filter;
+
+  nodeModules = stdenv.mkDerivation {
+    name = "playground-deps-${version}.tar.gz";
+    src = nix-filter.filter {
+      root = ./.;
+      include = [
+        ./package.json
+        ./pnpm-lock.yaml
+        "patches"
+      ];
+    };
+
+    nativeBuildInputs = [ nodePackages.pnpm ];
+    dontConfigure = true;
+
+    buildPhase = ''
+      runHook preBuild
+
+      export SOURCE_DATE_EPOCH=1
+      export NODE_EXTRA_CA_CERTS="${cacert}/etc/ssl/certs/ca-bundle.crt"
+      pnpm i --frozen-lockfile --node-linker=hoisted
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      rm node_modules/.modules.yaml
+      tar -czf $out --owner=0 --group=0 --numeric-owner --format=gnu \
+        --mtime="@$SOURCE_DATE_EPOCH" --sort=name --hard-dereference \
+        node_modules
+
+      runHook postInstall
+    '';
+
+    outputHashMode = "flat";
+    outputHashAlgo = "sha256";
+    outputHash = "sha256-y3N/4eN5uvaz48qvY8Hqyfg4zgko0JvIiFmuV82Ysfc=";
+  };
+in stdenv.mkDerivation {
+  pname = "playground-frontend";
+  inherit version;
   src = ./.;
 
-  nativeBuildInputs = [ nodePackages.pnpm ];
+  nativeBuildInputs = [ nodejs nodePackages.pnpm ];
   dontConfigure = true;
 
   buildPhase = ''
     runHook preBuild
 
-    export SOURCE_DATE_EPOCH=1
-    export NODE_EXTRA_CA_CERTS="${cacert}/etc/ssl/certs/ca-bundle.crt"
-    pnpm i --frozen-lockfile --node-linker=hoisted
+    tar xf ${nodeModules}
+    patchShebangs --build node_modules
+    pnpm build
 
     runHook postBuild
   '';
@@ -20,15 +66,10 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
 
-    rm node_modules/.modules.yaml
-    tar -czf $out --owner=0 --group=0 --numeric-owner --format=gnu \
-      --mtime="@$SOURCE_DATE_EPOCH" --sort=name --hard-dereference \
-      node_modules
+    cp -r public $out/public
+    cp -r .next/standalone $out
+    cp -r .next/static $out/.next/static
 
     runHook postInstall
   '';
-
-  outputHashMode = "flat";
-  outputHashAlgo = "sha256";
-  outputHash = "sha256-y3N/4eN5uvaz48qvY8Hqyfg4zgko0JvIiFmuV82Ysfc=";
 }
