a783d0a86df91e4d719fdc77e75204bbee956ebf
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-05-28 17:27:28 +0300

    Add react-compiler

---
+++ src/app/form/form.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useForm } from "react-hook-form";
+
+import { FormValues, getFullName } from "@/app/form/server";
+
+export default function Form() {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit = async (data: FormValues) => {
+    const result = await getFullName(data);
+    console.log(result);
+  };
+
+  return <form onSubmit={handleSubmit(onSubmit)}>
+    <input {...register('firstName')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="First name" />
+    <input {...register('lastName')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Last name" />
+    <button type="submit">Send</button>
+  </form>
+}
--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,24 +1,12 @@
-"use client"
-import { useForm } from "react-hook-form";
 import Link from "next/link";
 
-import { FormValues, getFullName } from "@/app/form/server";
+import Form from "@/app/form/form";
 
-export default function Form() {
-  const { register, handleSubmit } = useForm<FormValues>();
-
-  const onSubmit = async (data: FormValues) => {
-    const result = await getFullName(data);
-    console.log(result);
-  };
+export default function FormView() {
 
   return <div>
     <h1>Test</h1>
     <Link href="/">Back</Link>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="First name" />
-      <input {...register('lastName')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Last name" />
-      <button type="submit">Send</button>
-    </form>
+    <Form />
   </div>
 }
