d93cc7f75d71d4226de82a66d279c2970a468290
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-05-28 17:15:47 +0300

    Add back link

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { useForm } from "react-hook-form";
+import Link from "next/link";
+
 import { FormValues, getFullName } from "@/app/form/server";
 
 export default function Form() {
@@ -12,6 +14,7 @@ export default function Form() {
 
   return <div>
     <h1>Test</h1>
+    <Link href="/">Back</Link>
     <form onSubmit={handleSubmit(onSubmit)}>
       <input {...register('firstName')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="First name" />
       <input {...register('lastName')} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Last name" />
