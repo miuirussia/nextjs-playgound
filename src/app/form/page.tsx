b44089b29b72dcbc4643975ee54e393a1f608b55
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-05-29 12:43:26 +0000

    After usage

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import { unstable_after as after } from "next/server";
 
 import Form from "@/app/form/form";
 import List from "@/app/form/list";
 
 export default async function FormView() {
   const data = await fetch('https://jsonplaceholder.typicode.com/todos').then(d => d.json());
+
+  after(() => {
+    console.log('after');
+  })
 
   return <div>
     <h1>Test</h1>
