14af72acc8ec1913641a36ad985eba40b3723e07
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-07-16 16:37:51 +0300

    Fixes

--- src/lib/virtual.ts
+++ src/lib/virtual.ts
@@ -1,6 +1,6 @@
 import { useVirtualizer as useVirtualizerOriginal } from "@tanstack/react-virtual";
 
 export const useVirtualizer: typeof useVirtualizerOriginal = ((...args) => {
-  'use no memo'
-  return { ...useVirtualizerOriginal(...args) }
-}) as typeof useVirtualizerOriginal
+  "use no memo";
+  return { ...useVirtualizerOriginal(...args) };
+}) as typeof useVirtualizerOriginal;
