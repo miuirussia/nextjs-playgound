f7aabf606f7e67ff1d26778939a5a9adfcd1a955
Kirill Kuznetsov <kdevlab@yandex.ru>
2024-07-18 11:05:01 +0000

    Fix background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["variant", [
+    "@media (prefers-color-scheme: dark) { &:not(.light *) }",
+    "&:is(.dark *)",
+  ]],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
